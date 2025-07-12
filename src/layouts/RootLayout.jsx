2c348b2fa66329c197464cecfbcac28d545130c1
Jonibek Orifjonov  Web Developer <147488141+Jonibek011@users.noreply.github.com>
2025-07-12 23:20:46 +0500

    main section border olindi

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 function RootLayout() {
   return (
     <div className="w-full bg-slate-200 h-auto min-h-screen">
-      <main className="max-w-screen-lg mx-auto border">
+      <main className="max-w-screen-lg mx-auto ">
         <Outlet />
       </main>
     </div>
